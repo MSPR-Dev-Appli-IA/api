2618294bb7a29ff00112d2022745b77b65189c45
Ali <ali_saleh-4@hotmail.fr>
2023-04-23 21:20:52 +0200

    feat: upload image

--- src/controllers/species.controller.ts
+++ src/controllers/species.controller.ts
@@ -15,5 +15,15 @@ export const getSpecies  = async (req:Request, res:Response, _:NextFunction) => 
   };
 
 
+  export const createSpecies = async (req:Request, res:Response, _:NextFunction) => {
+    let { page=1, order, search } = req.body;
+    order = order == "ASC" ? 1 :-1
+    const skip:number =  limit * page - limit;
+
+    findLimitedSpecies(limit,skip,order,search)
+    res.status(404).json( "error" );
+    
+  };
+
 
 
--- src/routes/species.routes.ts
+++ src/routes/species.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import {getSpecies} from "../controllers/species.controller"
+import {getSpecies,createSpecies} from "../controllers/species.controller"
+import upload from '../config/image.config'
 
 const router = Router();
 
 import  {requireAuth} from "../middleware/AuthMiddleware";
 
 router.get("/",  requireAuth, getSpecies);
+router.post("/",  requireAuth,upload.array("files"), createSpecies);
 
 export default router;
