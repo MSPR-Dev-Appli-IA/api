e616a5c06f04eef2d07c8646993d828039032ecf
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-16 08:09:35 +0200

    feat(plantSitting): get plant sittings

--- src/queries/plantSitting.queries.ts
+++ src/queries/plantSitting.queries.ts
@@ -2,10 +2,22 @@ import { PlantSitting } from "../database/models/plantSitting.model";
 import { Plant } from "../database/models/plant.models";
 import { Types } from 'mongoose';
 import { IPlantSitting } from "../interfaces";
+import { Address } from "../database/models/adress.model";
 
 
 
 export const getOnePlantSittingById = async (plantSittingId: Types.ObjectId): Promise<IPlantSitting | null> => {
-    return await PlantSitting.findOne({ _id: plantSittingId }).populate({ path: "plant", model: Plant }).exec();
+    return await PlantSitting.findOne({ _id: plantSittingId }).populate({ path: "plant", model: Plant }).populate({ path: "address", model: Address }).exec();
 };
 
+export const findPlantSittingsNotTakenAndNotBegin = async () => {
+    const d =new Date();
+    return await PlantSitting.find({
+        start_at: {$gte: d.getTime()},
+        is_taken: false
+
+    })
+    .populate({ path: "plant", model: Plant })
+    .populate({ path: "address", model: Address })
+    .exec()
+};
