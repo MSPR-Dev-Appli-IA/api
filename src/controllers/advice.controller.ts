c9b9393ea3526943e2f22e6094fb00165d9e8808
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-17 19:26:28 +0100

    feat : getAdvicesNotTaken controller

--- src/controllers/advice.controller.ts
+++ src/controllers/advice.controller.ts
@@ -1,10 +1,20 @@
 
 import { NextFunction, Request, Response } from "express";
+import { findLimitedAdvicesNotTaken } from "../queries/advice.queries";
 
+const limit = 5
 
-export const getAdvicesNotTaken= async (_: Request, res: Response, __: NextFunction) => {
+export const getAdvicesNotTaken= async (req: Request, res: Response, __: NextFunction) => {
 
-    res.status(404).send({ message: "Error" });
+    try {
+        let { page=1, order } = req.body;
+        order = order == "ASC" ? 1 :-1
+        const skip:number =  limit * page - limit;
+        const advices = await findLimitedAdvicesNotTaken(limit,skip,order)
+        res.status(200).json( advices );
+      } catch (e) {
+        res.status(404).send({ message: "Error" });
+      }
   
 };
 
--- src/queries/advice.queries.ts
+++ src/queries/advice.queries.ts
@@ -12,3 +12,16 @@ export const getOneAdviceById = async (adviceId: Types.ObjectId): Promise<IAdvic
     .populate({ path: "image", model: Image })
     .populate({ path: "taked_by", model: User}).exec();
 };
+
+
+
+
+export const findLimitedAdvicesNotTaken = async ( limit: number = 1, skip: number = 0, order: 1 | -1 = -1) => {
+    return await Advice.find({
+        taked_by: null,
+    })
+        .populate({ path: "plant", model: Plant })
+        .populate({ path: "image", model: Image })
+        .sort({ created_at: order }).skip(skip).limit(limit)
+        .exec()
+};
