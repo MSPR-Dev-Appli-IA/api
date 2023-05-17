42cbb404cc50edd409aa6b1b7623e570c00dd297
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-17 19:49:00 +0100

    feat take one advice controller

--- src/middleware/adviceMiddleware.ts
+++ src/middleware/adviceMiddleware.ts
@@ -9,28 +9,29 @@ export const AreYouBotanistOrOwnerAdvice = async (req: Request, res: Response, n
         const adviceId = req.params.adviceId
         const advice = await getOneAdviceById(new mongoose.Types.ObjectId(adviceId.trim()))
         if (advice) {
-            const plant = await  getOnePlantById(advice.plant._id)
-            if (plant){
+            const plant = await getOnePlantById(advice.plant._id)
+            if (plant) {
                 const user = await findUserPerId(plant?.user._id)
-                if (req.user._id.equals(plant.user._id)|| user?.role.name== "Botanist") {
+                if (req.user._id.equals(plant.user._id) || user?.role.name == "Botanist") {
                     next()
                 } else {
                     res.status(404).send({ message: "Your are not allowed" });
                 }
-            }else{
+            } else {
                 res.status(404).send({ message: "Erreurs" });
             }
-          
+
         } else {
 
             res.status(404).send({ message: "Erreurs" });
         }
 
     } catch (error) {
-        
+
         res.status(404).send({ message: error });
     }
 };
+
 
 
 
@@ -39,27 +40,51 @@ export const areYouTheAdviceOwner = async (req: Request, res: Response, next: Ne
         const adviceId = req.params.adviceId
         const advice = await getOneAdviceById(new mongoose.Types.ObjectId(adviceId.trim()))
         if (advice) {
-            const plant = await  getOnePlantById(advice.plant._id)
-            if (plant){
+            const plant = await getOnePlantById(advice.plant._id)
+            if (plant) {
                 if (req.user._id.equals(plant.user._id)) {
                     next()
                 } else {
                     res.status(404).send({ message: "Your are not allowed" });
                 }
-            }else{
+            } else {
                 res.status(404).send({ message: "Erreurs" });
             }
-          
+
         } else {
 
             res.status(404).send({ message: "Erreurs" });
         }
 
     } catch (error) {
-        
+
         res.status(404).send({ message: error });
     }
 };
 
+
+export const notAlreadyTaken = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const adviceId = req.params.adviceId
+        const advice = await getOneAdviceById(new mongoose.Types.ObjectId(adviceId.trim()))
+        if (advice) {
+
+            if (!advice.taked_by) {
+                next()
+            } else {
+                res.status(404).send({ message: "Already taken" });
+            }
+
+
+        } else {
+
+            res.status(404).send({ message: "Erreurs" });
+        }
+
+    } catch (error) {
+
+        res.status(404).send({ message: error });
+    }
+};
 
 
