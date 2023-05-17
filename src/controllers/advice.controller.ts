f357b3ca5cb0e275953a96fb2271924e426e556e
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-17 19:16:22 +0100

    feat : advice routes

---
+++ src/controllers/advice.controller.ts
@@ -0,0 +1,43 @@
+
+import { NextFunction, Request, Response } from "express";
+
+
+export const getAdvicesNotTaken= async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+
+export const getMyAdvices = async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+
+export const getMyAdvicesBotanist = async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+export const getOneAdvice= async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+
+export const createAdvice = async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+
+export const takeOneAdvice = async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
+export const removeAdvice = async (_: Request, res: Response, __: NextFunction) => {
+
+    res.status(404).send({ message: "Error" });
+  
+};
---
+++ src/middleware/adviceMiddleware.ts
@@ -0,0 +1,65 @@
+import { NextFunction, Request, Response } from "express";
+import { getOnePlantById } from "../queries/plant.queries";
+import mongoose from 'mongoose';
+import { getOneAdviceById } from "../queries/advice.queries";
+import { findUserPerId } from "../queries/user.queries";
+
+export const AreYouBotanistOrOwnerAdvice = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const adviceId = req.params.adviceId
+        const advice = await getOneAdviceById(new mongoose.Types.ObjectId(adviceId.trim()))
+        if (advice) {
+            const plant = await  getOnePlantById(advice.plant._id)
+            if (plant){
+                const user = await findUserPerId(plant?.user._id)
+                if (req.user._id.equals(plant.user._id)|| user?.role.name== "Botanist") {
+                    next()
+                } else {
+                    res.status(404).send({ message: "Your are not allowed" });
+                }
+            }else{
+                res.status(404).send({ message: "Erreurs" });
+            }
+          
+        } else {
+
+            res.status(404).send({ message: "Erreurs" });
+        }
+
+    } catch (error) {
+        
+        res.status(404).send({ message: error });
+    }
+};
+
+
+
+export const areYouTheAdviceOwner = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const adviceId = req.params.adviceId
+        const advice = await getOneAdviceById(new mongoose.Types.ObjectId(adviceId.trim()))
+        if (advice) {
+            const plant = await  getOnePlantById(advice.plant._id)
+            if (plant){
+                if (req.user._id.equals(plant.user._id)) {
+                    next()
+                } else {
+                    res.status(404).send({ message: "Your are not allowed" });
+                }
+            }else{
+                res.status(404).send({ message: "Erreurs" });
+            }
+          
+        } else {
+
+            res.status(404).send({ message: "Erreurs" });
+        }
+
+    } catch (error) {
+        
+        res.status(404).send({ message: error });
+    }
+};
+
+
+
