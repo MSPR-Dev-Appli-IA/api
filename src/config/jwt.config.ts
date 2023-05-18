47e64600b2524202bcc2d61c31650e327b8e7b2e
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-05-18 15:52:26 +0200

    fix(Auth): Add cors headers

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -4,7 +4,7 @@ import  {app} from "../index";
 import { IUser,UserjwtToken } from "../interfaces";
 import { NextFunction, Request, Response } from "express";
 
-const key = process.env.API_KEY ? process.env.API_KEY : ""
+const key = process.env.JWTKEY ? process.env.JWTKEY : ""
 const createJwtToken = ({ user, id }:UserjwtToken) => {
   
   const jwtToken = jwt.sign(
@@ -33,7 +33,7 @@ const checkExpirationToken = (token:JwtPayload, res:Response) => {
     return token;
   } else if (nowInSec > tokenExp && nowInSec - tokenExp < 60 * 60 * 24) {
     const refreshedToken = createJwtToken({user: null,id: token.sub });
-    res.cookie("jwt", refreshedToken);
+    res.cookie("jwt", refreshedToken, { sameSite: 'none', secure: true});
     return jwt.verify(refreshedToken, key);
   } else {
     throw new Error("token expired");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express ,{Application,  Request, Response} from "express";
+import express, {Application, NextFunction, Request, Response} from "express";
 import {join} from 'path';
 import  cookieParser from 'cookie-parser';
 import  router from './routes';
@@ -19,6 +19,29 @@ app.use(compression()); // Compress all routes
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(function (_req:Request, res:Response, next:NextFunction) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    // Pass to next layer of middleware
+    next();
+});
 
 import "./config/jwt.config";
 
