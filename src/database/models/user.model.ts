e28cb07fa23986d8a8d011c838bad57636e55e38
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-05-18 16:43:46 +0200

    refactor(Auth): Refactor User Model

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,34 +1,35 @@
-import  mongoose from 'mongoose';
-import  bcrypt from "bcrypt";
-import { UserModel,IUser } from '../../interfaces';
+import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+import {IUser, UserModel} from '../../interfaces';
+
 const schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema<IUser,UserModel>({
-    username: { type: String, required: true, unique: true },
-    firstname: { type: String, required: true},
-    lastname: { type: String, required: true },
-    created_at: {type: Date,require: true,default: Date.now()},
-    deleted_at: {type: Date,require: false},
-    role: { type: schema.Types.ObjectId, ref: "Role", required: true },
-    image: { type: schema.Types.ObjectId, ref: "Image", required: false,default:null },
+const userSchema = new mongoose.Schema<IUser, UserModel>({
+    username: {type: String, required: true, unique: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    created_at: {type: Date, require: true, default: Date.now()},
+    deleted_at: {type: Date, require: false},
+    role: {type: schema.Types.ObjectId, ref: "Role", required: true},
+    image: {type: schema.Types.ObjectId, ref: "Image", required: false, default: null},
     local: {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+    },
+
 });
 
-userSchema.statics.hashPassword = async (password:string) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    return bcrypt.hash(password, salt);
-  } catch (e) {
-    throw e;
-  }
+userSchema.statics.hashPassword = async (password: string) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        return bcrypt.hash(password, salt);
+    } catch (e) {
+        throw e;
+    }
 };
 
-userSchema.methods.comparePassword = function (password:string) {
-  return bcrypt.compareSync(password, this.local.password);
+userSchema.methods.comparePassword = function (password: string) {
+    return bcrypt.compareSync(password, this.local.password);
 };
 
 export const User = mongoose.model<IUser, UserModel>('User', userSchema);
