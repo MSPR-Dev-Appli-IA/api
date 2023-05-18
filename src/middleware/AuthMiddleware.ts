a250a1df381ad6be8d7d31c598aec4fbbf2b4955
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-05-18 15:09:44 +0200

    fix(Auth): Change the http response when the user is still logged in and tries to create or register a new account.

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -12,7 +12,7 @@ export const notRequireAuth = (req:Request, res:Response, next:NextFunction) => 
   if (!req.isAuthenticated()) {
     next();
   } else {
-    res.status(404).send({ message: "Your are  logged in" });
+    res.status(304).send({ field: ["error"], "message": ["Your are always logged"] });
   }
 };
 
