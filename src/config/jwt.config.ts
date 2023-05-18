566b435b94f27efe5fba20986bfa88fb71119757
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-05-18 16:02:39 +0200

    fix(Auth): Update JWT http responses

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -36,34 +36,41 @@ const checkExpirationToken = (token:JwtPayload, res:Response) => {
     res.cookie("jwt", refreshedToken, { sameSite: 'none', secure: true});
     return jwt.verify(refreshedToken, key);
   } else {
-    throw new Error("token expired");
+    res.clearCookie("jwt");
+    res.status(401).send({
+      field: ["error"],
+      message: ["Token expired."]
+    })
   }
 }
 return;
 };
 
-const extractUserFromToken = async (req:Request, res:Response, next:NextFunction) => {
+const extractUserFromToken = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.jwt;
   if (token) {
     try {
-      const  decodedToken   = jwt.verify(token, key, { ignoreExpiration: true }) as JwtPayload ;
-      const decodedTokenCheck = checkExpirationToken(decodedToken  , res);
-      if (decodedTokenCheck){
-      const user = await findUserPerId(decodedTokenCheck.sub as string);
-      if (user) {
-        req.user = user;
-        next();
-      } else {
-        res.clearCookie("jwt");
-        res.json(null)
-      }
-      }else{
-        res.clearCookie("jwt");
-        res.json(null)
+      const decodedToken = jwt.verify(token, key, {ignoreExpiration: true}) as JwtPayload;
+      const decodedTokenCheck = checkExpirationToken(decodedToken, res);
+      if (decodedTokenCheck) {
+        const user = await findUserPerId(decodedTokenCheck.sub as string);
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.clearCookie("jwt");
+          res.status(404).send({
+            field: ["error"],
+            message: ["User not Found. Please contact us."]
+          })
+        }
       }
     } catch (e) {
       res.clearCookie("jwt");
-      res.json(null)
+      res.status(500).send({
+        field: ["error"],
+        message: ["An error was occurred. Please contact us"]
+      })
     }
   } else {
     next();
