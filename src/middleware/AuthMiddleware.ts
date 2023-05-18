47dc108d4a5f77994c6e7a22eadee477452c587c
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-05-18 15:13:07 +0200

    fix(Auth): Change the http response and message when the user is not a Botanist or Admin

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -12,7 +12,7 @@ export const notRequireAuth = (req:Request, res:Response, next:NextFunction) => 
   if (!req.isAuthenticated()) {
     next();
   } else {
-    res.status(304).send({ field: ["error"], "message": ["Your are always logged"] });
+    res.status(304).send({ field: ["error"], message: ["Your are always logged."] });
   }
 };
 
@@ -22,7 +22,7 @@ export const isItBotanist = (req:Request, res:Response, next:NextFunction) => {
   if (req.user.role.name =="Botanist" || req.user.role.name =="Admin") {
     next();
   } else {
-    res.status(404).send( { message: "Your are not allowed" });
+    res.status(401).send( { field: ["error"], message: ["Bad token or Invalid Permission."] });
   }
 };
 
