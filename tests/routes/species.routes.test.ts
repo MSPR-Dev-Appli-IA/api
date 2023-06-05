a4e6da63b6a614c586eb31da1bb5aa6bfa132831
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-06-05 22:53:48 +0200

    test(Species): Add images & delete species Tests

--- tests/routes/species.routes.test.ts
+++ tests/routes/species.routes.test.ts
@@ -146,194 +146,35 @@ describe("get species", () => {
 
     });
 });
-//
-//
-// describe("species and image ", () => {
-//   let speciesIdToGet:String|null = null
-//   let arrayPathImage:IImage[]= []
-//   beforeAll(async()=>{
-//
-//      const resp = await request(app).post("/api/species")
-//       .set('Content-type', 'application/json')
-//       .set('Cookie', cookieJWTBotanist)
-//       .send({ "name":"test_image",
-//       "description":"desscription test image ",
-//       "sunExposure":"sun exposure test image 9",
-//       "watering":"watering test image ",
-//       "optimalTemperature":"optimal temperature test image "
-//    })
-//
-//    speciesIdToGet = resp.body._id
-//
-//     });
-//
-//
-//     test("add image to a species ", async () => {
-//       const resp = await request(app).post("/api/species/addImage/"+speciesIdToGet)
-//         .set('Content-Type','multipart/form-data')
-//         .set('Cookie', cookieJWTBotanist)
-//         .attach('file', `public/testImage/rosa.jpg`)
-//         expect(resp.statusCode).toBe(200)
-//         expect(resp.body.name).toEqual("test_image")
-//         expect(resp.body.images.length).toEqual(1)
-//         expect(fs.existsSync("public/image/" + resp.body.images[0].path)).toBeTruthy()
-//         arrayPathImage.push(resp.body.images[0])
-//     });
-//
-//     test("add image to a species without botanist account ", async () => {
-//       const resp = await request(app).post("/api/species/addImage/"+speciesIdToGet)
-//         .set('Content-Type','multipart/form-data')
-//         .set('Cookie', cookieJWTUser)
-//         .attach('file', `public/testImage/rosa.jpg`)
-//         expect(resp.statusCode).toBe(404)
-//
-//     });
-//
-//
-//
-//     test("add another image to a species ", async () => {
-//       const resp = await request(app).post("/api/species/addImage/"+speciesIdToGet)
-//         .set('Content-Type','multipart/form-data')
-//         .set('Cookie', cookieJWTBotanist)
-//         .attach('file', `public/testImage/rosa.jpg`)
-//         expect(resp.statusCode).toBe(200)
-//         expect(resp.body.name).toEqual("test_image")
-//         expect(resp.body.images.length).toEqual(2)
-//         expect(fs.existsSync("public/image/" + resp.body.images[1].path)).toBeTruthy()
-//         arrayPathImage.push(resp.body.images[1])
-//     });
-//
-//     test("add another image to a species ", async () => {
-//       const resp = await request(app).post("/api/species/addImage/"+speciesIdToGet)
-//         .set('Content-Type','multipart/form-data')
-//         .set('Cookie', cookieJWTBotanist)
-//         .attach('file', `public/testImage/rosa.jpg`)
-//         expect(resp.statusCode).toBe(200)
-//         expect(resp.body.name).toEqual("test_image")
-//         expect(resp.body.images.length).toEqual(3)
-//         expect(fs.existsSync("public/image/" + resp.body.images[2].path)).toBeTruthy()
-//         arrayPathImage.push(resp.body.images[2])
-//     });
-//
-//
-//     test(" delete one image to a species ", async () => {
-//       const resp = await request(app).delete("/api/species/deleteImage/"+speciesIdToGet+"/"+arrayPathImage[0]._id)
-//         .set('Cookie', cookieJWTBotanist)
-//         expect(resp.statusCode).toBe(200)
-//         expect(resp.body.name).toEqual("test_image")
-//         expect(resp.body.images.length).toEqual(2)
-//         expect(fs.existsSync("public/image/" + arrayPathImage[0].path)).toBeFalsy()
-//     });
-//
-//     test(" delete one image to a species without botanist account ", async () => {
-//       const resp = await request(app).delete("/api/species/deleteImage/"+speciesIdToGet+"/"+arrayPathImage[0]._id)
-//         .set('Cookie', cookieJWTUser)
-//         expect(resp.statusCode).toBe(404)
-//
-//     });
-//
-//     test(" delete one image to a species ", async () => {
-//       const resp = await request(app).delete("/api/species/deleteImage/"+speciesIdToGet+"/"+arrayPathImage[1]._id)
-//         .set('Cookie', cookieJWTBotanist)
-//         expect(resp.statusCode).toBe(200)
-//         expect(resp.body.name).toEqual("test_image")
-//         expect(resp.body.images.length).toEqual(1)
-//         expect(fs.existsSync("public/image/" + arrayPathImage[1].path)).toBeFalsy()
-//     });
-//
-//
-//     test(" delete one species without botanist token ", async () => {
-//
-//       const resp = await request(app).delete("/api/species/"+speciesIdToGet)
-//         .set('Cookie', cookieJWTUser)
-//         expect(resp.statusCode).toBe(404)
-//
-//     });
-//
-//     test(" delete one species ", async () => {
-//
-//       const resp = await request(app).delete("/api/species/"+speciesIdToGet)
-//         .set('Cookie', cookieJWTBotanist)
-//         expect(resp.statusCode).toBe(200)
-//         expect(fs.existsSync("public/image/" + arrayPathImage[2].path)).toBeFalsy()
-//     });
-//
-//     test("get one species deleted", async () => {
-//
-//       const resp = await request(app).get("/api/species/"+speciesIdToGet)
-//         .set('Content-type', 'application/json')
-//         .set('Cookie', cookieJWTUser)
-//         expect(resp.statusCode).toBe(404)
-//
-//
-//     });
-//
-//
-//
-//
-//   })
-//
-//
-//
-//   describe("update species  ", () => {
-//     let speciesIdToGet:String|null = null
-//     beforeAll(async()=>{
-//        const resp = await request(app).post("/api/species")
-//         .set('Content-type', 'application/json')
-//         .set('Cookie', cookieJWTBotanist)
-//         .send({ "name":"test_update",
-//         "description":"desscription test update ",
-//         "sunExposure":"sun exposure test update",
-//         "watering":"watering test update ",
-//         "optimalTemperature":"optimal temperature test update "
-//      })
-//
-//      speciesIdToGet = resp.body._id
-//
-//       });
-//
-//       const upload_payload ={ "name":"test_update_after",
-//       "description":"desscription test update ",
-//       "sunExposure":"sun exposure test update after ",
-//       "watering":"watering test update ",
-//       "optimalTemperature":"optimal temperature test update "
-//    }
-//
-//       test("update species  ", async () => {
-//         const resp = await request(app).post("/api/species/"+speciesIdToGet)
-//          .set('Content-type', 'application/json')
-//           .set('Cookie', cookieJWTBotanist)
-//           .send(upload_payload)
-//           expect(resp.statusCode).toBe(200)
-//           expect(resp.body).toEqual(expect.objectContaining(upload_payload))
-//
-//       });
-//
-//       test("update species with same name   ", async () => {
-//         const resp = await request(app).post("/api/species/"+speciesIdToGet)
-//          .set('Content-type', 'application/json')
-//           .set('Cookie', cookieJWTBotanist)
-//           .send({ "name":"Rosa",
-//           "description":"desscription test update ",
-//           "sunExposure":"sun exposure test update after ",
-//           "watering":"watering test update ",
-//           "optimalTemperature":"optimal temperature test update "
-//        })
-//           expect(resp.statusCode).toBe(404)
-//
-//       });
-//
-//       test("update species without botanist account", async () => {
-//         const resp = await request(app).post("/api/species/"+speciesIdToGet)
-//         .set('Content-type', 'application/json')
-//           .set('Cookie', cookieJWTUser)
-//           .send(upload_payload)
-//           expect(resp.statusCode).toBe(404)
-//
-//       });
-//
-//
-//
-//
-//
-//     })
+
+describe("add & delete species images", () => {
+
+    test("add image to a species ", async () => {
+        const resp = await request("https://api-arosaje-test.locascio.fr").post("/api/species/addImage")
+  -          .set('Content-Type', 'multipart/form-data')
+            .set('Authorization', 'Bearer ' + botanistInfo["JWTBotanist"])
+            .attach('file', `public/testImage/rosa.jpg`)
+            .field("speciesId", mySpeciesId[0]._id)
+        expect(resp.statusCode).toBe(200)
+        expect(resp.body.status).toEqual("success")
+    });
+
+
+    test("add another image to a plant ", async () => {
+        const resp = await request("https://api-arosaje-test.locascio.fr").post("/api/species/addImage")
+            .set('Content-Type', 'multipart/form-data')
+            .set('Authorization', 'Bearer ' + botanistInfo["JWTBotanist"])
+            .attach('file', `public/testImage/rosa.jpg`)
+            .field("speciesId", mySpeciesId[0]._id)
+        expect(resp.statusCode).toBe(200)
+        expect(resp.body.status).toEqual("success")
+    });
+
+    test("delete species", async () => {
+        const resp = await request("https://api-arosaje-test.locascio.fr").delete("/api/species/" + mySpeciesId[1]._id)
+            .set('Content-type', 'application/json')
+            .set('Authorization', 'Bearer ' + botanistInfo["JWTBotanist"])
+        expect(resp.statusCode).toBe(200)
+        expect(resp.body).toEqual({type: "success", message: "Plant deleted"})
+    })
+})
