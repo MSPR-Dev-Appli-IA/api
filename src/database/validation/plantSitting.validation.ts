37a3a3883115470e4b5c6b5c8c8f24d10cecaf5c
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-07-02 22:59:44 +0200

    fix(plantSitting): Fix Update an plantSitting to respect HTTP convention

--- src/database/validation/plantSitting.validation.ts
+++ src/database/validation/plantSitting.validation.ts
@@ -17,4 +17,24 @@ export const plantSittingValidation = Joi.object({
     y: Joi.number()
   })
 });
+
+export const updateplantSittingValidation = Joi.object({
+  plantSittingId: Joi.string().length(24).required(),
+  plantId: Joi.string().length(24).required(),
+  description: Joi.string().min(2).required().messages({
+    "string.base": "La description valide",
+
+  }),
+  start_at: Joi.date().min(Date.now()).required().messages({
+    "string.base": "La date de de debut n'est pas valide",
+
+  }),
+  end_at: Joi.date().min(Joi.ref('start_at')).required().messages({
+    "string.base": "La datez  de fin n'est pas valide",
+  }),
+  address: Joi.object({
+    x: Joi.number(),
+    y: Joi.number()
+  })
+});
   
